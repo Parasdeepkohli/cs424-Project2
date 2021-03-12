add76d7a263350d120480dfa488ac180359b2d18
Parasdeep <56748097+Parasdeepkohli@users.noreply.github.com>
2021-03-12 23:07:05 +0530

    Added basic map structure' wq

--- server.R
+++ server.R
@@ -2,10 +2,34 @@ library(shiny)
 library(ggplot2)
 library(DT)
 library(scales)
-library(usmap)
+library(leaflet)
 library(stringr)
 
 function(input, output){
   
+  source("Preprocessing.R")
+  input_data_2018 <- preprocess("Input_data_2018.csv")
+
+  # getColor <- function(df) {
+  #   sapply(df$, function(mag) {
+  #     if(mag <= 4) {
+  #       "green"
+  #     } else if(mag <= 5) {
+  #       "orange"
+  #     } else {
+  #       "red"
+  #     } })
+  # }
+  # 
+  
+  Illinois_data <- subset(input_data_2018, PSTATEABB == "IL")
+  
+  output$map <- renderLeaflet({
+    
+    leaflet(Illinois_data) %>%
+    addTiles() %>%
+    addCircleMarkers(label = ~as.character(PNAME))
+    
+  })
   
 }
